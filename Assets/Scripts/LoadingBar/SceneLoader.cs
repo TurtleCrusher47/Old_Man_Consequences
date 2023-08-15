cd919b53bcd4b7788cccfb5e7997fece816413fc
NoahForTW <111335518+NoahForTW@users.noreply.github.com>
2023-08-15 15:55:36 +0800

    Day2 Fix Stuff

---
+++ Assets/Scripts/LoadingBar/SceneLoader.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void NewGame(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void LoadGame(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void ExitScene()
+    {
+        Application.Quit();
+    }
+}
