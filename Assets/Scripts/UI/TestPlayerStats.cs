6c747949dfb297ed8d86c2cdb8c8aa5e92e28f48
NoahForTW <yiphanwen3@yahoo.com>
2023-08-16 23:39:54 +0800

    Updated the Thirst and Hunger UI

--- Assets/Scripts/UI/TestPlayerStats.cs
+++ Assets/Scripts/UI/TestPlayerStats.cs
@@ -10,43 +10,38 @@ public class TestPlayerStats : MonoBehaviour
     [SerializeField] private Image hungerBar;
     [SerializeField] private float maxHunger = 100f;
     [SerializeField] private float hungerDecreaseAmountOnClick = 5f; // Decrease hunger on mouse click
+    [SerializeField] private float hungerDecreaseInterval = 8f; // Decrease energy every 8 seconds
+    [SerializeField] private float hungerDecreasePerSecond = 10f;
 
     [Header("Thirst")]
     [SerializeField] private Image thirstBar;
-    [SerializeField] private float maxThirst = 150f; // Increase max thirst to 150f
+    [SerializeField] private float maxThirst = 100f; // Increase max thirst to 100f
     [SerializeField] private float thirstDecreaseInterval = 5f; // Decrease thirst every 5 seconds
     [SerializeField] private float thirstDecreaseAmount = 5f;
 
     // Scrap idea but c first
+    /*
     [Header("Energy")]
     [SerializeField] private TMP_Text energyProgressText;
     [SerializeField] private Slider energySlider;
     [SerializeField] private float energyDecreaseInterval = 8f; // Decrease energy every 8 seconds
-    [SerializeField] private float energyDecreaseAmount = 10f;
+    [SerializeField] private float energyDecreaseAmount = 10f;*/
 
     private float nextThirstDecreaseTime;
-    private float nextEnergyDecreaseTime;
-
-    private float initialHunger;
-    private float initialThirst;
-    private float initialEnergy;
+    private float nextHungerDecreaseTime;
 
     // Start is called before the first frame update
     void Start()
     {
         nextThirstDecreaseTime = Time.time + thirstDecreaseInterval;
-        nextEnergyDecreaseTime = Time.time + energyDecreaseInterval;
-
-        initialHunger = maxHunger;
-        initialThirst = maxThirst;
-        initialEnergy = energySlider.maxValue;  
+        nextHungerDecreaseTime = Time.time + hungerDecreasePerSecond;
     }
 
     // Update is called once per frame
     void Update()
     {
         // Handle hunger decrease on mouse click
-        if (Input.GetKeyDown(KeyCode.KeypadEnter))
+        if (Input.GetKeyDown(KeyCode.Return))
         {
             DecreaseHunger(hungerDecreaseAmountOnClick);
         }
@@ -60,14 +55,15 @@ public class TestPlayerStats : MonoBehaviour
         // Handle Test reset of players Thirst
         if (Input.GetKeyDown(KeyCode.T))
         {
-            PlayerThirstRefill(30);
+            PlayerThirstRefill();
         }
 
         // Handle Test reset of players Energy
+        /*
         if (Input.GetKeyDown(KeyCode.E))
         {
             PlayerEnergyRefill();
-        }
+        }*/
 
         // Handle thirst decrease over time
         if (Time.time >= nextThirstDecreaseTime)
@@ -76,35 +72,37 @@ public class TestPlayerStats : MonoBehaviour
             nextThirstDecreaseTime = Time.time + thirstDecreaseInterval;
         }
 
-        // Handle energy decrease over time
-        if (Time.time >= nextEnergyDecreaseTime)
+        // Handle hunger decrease over time
+        if (Time.time >= nextHungerDecreaseTime)
         {
-            DecreaseEnergy(energyDecreaseAmount);
-            nextEnergyDecreaseTime = Time.time + energyDecreaseInterval;
+            DecreaseHunger(hungerDecreasePerSecond);
+            nextHungerDecreaseTime = Time.time + hungerDecreaseInterval;
         }
     }
 
     private void PlayerHungerRefill()
     {
-        hungerBar.fillAmount = initialHunger / maxHunger;
+        hungerBar.fillAmount = maxHunger;
     }
 
-    private void PlayerThirstRefill(float amount)
+    private void PlayerThirstRefill()
     {
-        thirstBar.fillAmount = initialThirst / amount;
-        // Refill thirst by 30 
+        float newThirstValue = Mathf.Clamp(thirstBar.fillAmount + (30f / maxThirst), 0f, 1f);
+        thirstBar.fillAmount = newThirstValue;
     }
 
+    /*
     private void PlayerEnergyRefill()
     {
         energySlider.value = initialEnergy;
         energyProgressText.text = $"{initialEnergy} %";
-    }
+    } */
 
     private void DecreaseHunger(float amount)
     {
         // Update hunger UI
-        hungerBar.fillAmount -= amount / maxHunger;
+        float newHungerValue = Mathf.Clamp(hungerBar.fillAmount - (amount / maxHunger), 0f, 1f);
+        hungerBar.fillAmount = newHungerValue;
     }
 
     private void DecreaseThirst(float amount)
@@ -113,10 +111,11 @@ public class TestPlayerStats : MonoBehaviour
         thirstBar.fillAmount -= amount / maxThirst; 
     }
 
+    /*
     private void DecreaseEnergy(float amount)
     {
         // Update energy UI
         energySlider.value -= amount;     
         energyProgressText.text = $"{energySlider.value} %";
-    }
+    } */
 }
