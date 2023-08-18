f303f5a865f030059308ec0e61737510bfb99a13
TurtleCrusher47 <111266586+TurtleCrusher47@users.noreply.github.com>
2023-08-18 15:10:52 +0800

    Added balance

--- Assets/Scripts/Shop/ShopTemplate.cs
+++ Assets/Scripts/Shop/ShopTemplate.cs
@@ -1,18 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+//using Microsoft.Unity.VisualStudio.Editor;
 
 public class ShopTemplate : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public TMP_Text name;
+    public TMP_Text description;
+    public TMP_Text cost;
+    public Image sprite;
 }
