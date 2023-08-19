06771e183d3e84ce27130d16cb8d52f98e446438
bailuob0 <tanialam1101@gmail.com>
2023-08-20 03:46:22 +0800

    Shop UI tied in with inventory system!
    -shop UI now shows items based on purchaseable item SOs added
    -purchasing items from shop now adds them to your inventory!

--- Assets/Scripts/Shop/ShopManager.cs
+++ Assets/Scripts/Shop/ShopManager.cs
@@ -1,15 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class ShopManager : MonoBehaviour
 {
     [SerializeField] PlayerData playerData;
+    [SerializeField] InventorySO inventoryData;
 
     public TMP_Text balance;
-    // public PurchaseableItemSO[] purchaseableItemSO;
+    public PurchaseableItemSO[] purchaseableItemSO;
     public GameObject[] shopPanelGO;
     public ShopTemplate[] shopPanel;
     public Button[] purchaseButtons;
@@ -17,50 +19,60 @@ public class ShopManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        // for (int i = 0; i < purchaseableItemSO.Length; i ++)
-        // {
-        //     shopPanelGO[i].SetActive(true);
-        // }
+        for (int i = 0; i < purchaseableItemSO.Length; i ++)
+        {
+            shopPanelGO[i].SetActive(true);
+        }
 
-        UpdateBalance();
         LoadPanels();
+        UpdateBalance();
         CheckPurchaseable();
     }
 
     // Call on start and on sell
     public void CheckPurchaseable()
     {
-        // for (int i = 0; i < purchaseableItemSO.Length; i ++)
-        // {
-        //     if (playerData.Balance >= purchaseableItemSO[i].price)
-        //     {
-        //         purchaseButton[i].interactable = true;
-        //     }
-        //     else
-        //     {
-        //         purchaseButton[i].interactable = false;
-        //     }
-        // }
+        for (int i = 0; i < purchaseableItemSO.Length; i ++)
+        {
+            if (playerData.Balance >= purchaseableItemSO[i].PurchasePrice)
+            {
+                purchaseButtons[i].interactable = true;
+            }
+            else
+            {
+                purchaseButtons[i].interactable = false;
+            }
+        }
     }
 
     public void PurchaseItem(int buttonNumber)
     {
-        // if (playerData.Balance >= purchaseableItemSO[buttonNumber].price)
-        // {
-        //     playerData.Balance -= purchaseableItemSO[buttonNumber].price;
-        //     UpdateBalance();
-        //     CheckPurchaseable();
-        // }
+        if (playerData.Balance >= purchaseableItemSO[buttonNumber].PurchasePrice)
+        {
+            playerData.Balance -= purchaseableItemSO[buttonNumber].PurchasePrice;
+
+            inventoryData.AddItem(purchaseableItemSO[buttonNumber], 1);
+
+            //add this code back when we add the inventory to the scene
+            //inventoryUI is an InventoryPage instance that should be serialized
+            // foreach (var item in inventoryData.GetCurrentInventoryState())
+            // {
+            //     inventoryUI.UpdateData(item.Key, item.Value.item.ItemImage, item.Value.quantity);
+            // }
+            UpdateBalance();
+            CheckPurchaseable();
+        }
     }
 
     public void LoadPanels()
     {
-        // for (int i = 0; i < purchaseableItemSO.Length; i ++)
-        // {
-        //     shopPanel[i].name.text = purchasableItemSO[i].name;
-        //     shopPanel[i].description.text = purchasableItemSO[i].description;
-        //     shopPanel[i].price.text = "$" + purchasableItemSO[i].cost.ToString();
-        // }
+        for (int i = 0; i < purchaseableItemSO.Length; i++)
+        {
+            shopPanel[i].name.text = purchaseableItemSO[i].Name;
+            shopPanel[i].price.text = "$" + purchaseableItemSO[i].PurchasePrice.ToString();
+            shopPanel[i].sprite.sprite = purchaseableItemSO[i].ItemImage;
+            shopPanel[i].hoverTip.tipToShow = purchaseableItemSO[i].Description;
+        }
     }
 
     public void UpdateBalance()
