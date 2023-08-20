8f0d855a4d42f3674e8587ba6da28582f37c23d2
TurtleCrusher47 <111266586+TurtleCrusher47@users.noreply.github.com>
2023-08-20 20:39:00 +0800

    Basic shop selling logic done

--- Assets/Scripts/Shop/ShopManager.cs
+++ Assets/Scripts/Shop/ShopManager.cs
@@ -11,20 +11,37 @@ public class ShopManager : MonoBehaviour
     [SerializeField] InventorySO inventoryData;
 
     public TMP_Text balance;
+    
     public PurchaseableItemSO[] purchaseableItemSO;
-    public GameObject[] shopPanelGO;
-    public ShopTemplate[] shopPanel;
+    public GameObject[] buyShopPanelGO;
+    public ShopTemplate[] buyShopPanel;
     public Button[] purchaseButtons;
+
+    public SellableItemSO[] sellableItemSO;
+    public GameObject[] sellShopPanelGO;
+    public ShopTemplate[] sellShopPanel;
+    public Button[] sellButtons;
+
+    void Awake()
+    {
+        LoadInventoryItems();
+    }
     
     // Start is called before the first frame update
     void Start()
     {
         for (int i = 0; i < purchaseableItemSO.Length; i ++)
         {
-            shopPanelGO[i].SetActive(true);
+            buyShopPanelGO[i].SetActive(true);
         }
 
-        LoadPanels();
+        for (int i = 0; i < sellableItemSO.Length; i ++)
+        {
+            sellShopPanelGO[i].SetActive(true);
+        }
+
+        LoadPurchaseablePanels();
+        LoadSellablePanels();
         UpdateBalance();
         CheckPurchaseable();
     }
@@ -45,6 +62,17 @@ public class ShopManager : MonoBehaviour
         }
     }
 
+    public void CheckSellable()
+    {
+        // Check whether the player still has any of the items left
+
+        // else
+        // {
+        //     sellableButtons[i].interactable = false;
+        // }
+    }
+
+    // Purchase the item
     public void PurchaseItem(int buttonNumber)
     {
         if (playerData.Balance >= purchaseableItemSO[buttonNumber].PurchasePrice)
@@ -64,14 +92,54 @@ public class ShopManager : MonoBehaviour
         }
     }
 
-    public void LoadPanels()
+    // Sell the item
+    public void SellItem(int buttonNumber)
+    {
+        // For Tania: delete the item, add to player's balance
+        UpdateBalance();
+        CheckSellable();
+    }
+
+    public void SellAllItems()
+    {
+        // For each sellable item in the inventory, remove it from inventory and add to player balance
+        
+        UpdateBalance();
+        CheckSellable();
+    }
+
+    // Load a panel for each of the purchasable items
+    public void LoadPurchaseablePanels()
     {
         for (int i = 0; i < purchaseableItemSO.Length; i++)
         {
-            shopPanel[i].name.text = purchaseableItemSO[i].Name;
-            shopPanel[i].price.text = "$" + purchaseableItemSO[i].PurchasePrice.ToString();
-            shopPanel[i].sprite.sprite = purchaseableItemSO[i].ItemImage;
-            shopPanel[i].hoverTip.tipToShow = purchaseableItemSO[i].Description;
+            buyShopPanel[i].name.text = purchaseableItemSO[i].Name;
+            buyShopPanel[i].price.text = "$" + purchaseableItemSO[i].PurchasePrice.ToString();
+            buyShopPanel[i].sprite.sprite = purchaseableItemSO[i].ItemImage;
+            buyShopPanel[i].hoverTip.tipToShow = purchaseableItemSO[i].Description;
+        }
+    }
+
+    // Loop through the player's inventory and add the fish to the list of sellable items
+    public void LoadInventoryItems()
+    {
+        // For Tania: loop through the inventory, check if the item is sellable, if it is, add it to the list
+
+        // for (int i = 0; i < inventoryList.Length; i++)
+        // {
+
+        // }
+    }
+
+    // Load a panel for each of the sellable items
+    public void LoadSellablePanels()
+    {
+        for (int i = 0; i < sellableItemSO.Length; i++)
+        {
+            sellShopPanel[i].name.text = sellableItemSO[i].Name;
+            sellShopPanel[i].price.text = "$" + sellableItemSO[i].SellPrice.ToString();
+            sellShopPanel[i].sprite.sprite = sellableItemSO[i].ItemImage;
+            sellShopPanel[i].hoverTip.tipToShow = sellableItemSO[i].Description;
         }
     }
 
