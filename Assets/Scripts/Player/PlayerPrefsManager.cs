f1801055616e274f1b245ee2915a109137efd0a2
NoahForTW <yiphanwen3@yahoo.com>
2023-08-21 22:23:11 +0800

    Added Save and Load function

--- Assets/Scripts/Player/PlayerPrefsManager.cs
+++ Assets/Scripts/Player/PlayerPrefsManager.cs
@@ -4,12 +4,12 @@ public static class PlayerPrefsManager
 {
     public static void Save(string key, float value)
     {
-
+        PlayerPrefs.SetFloat(key, value);
+        PlayerPrefs.Save();
     }
 
-    /*
     public static float Load(string key)
     {
-
-    }*/
+        return PlayerPrefs.GetFloat(key);
+    }
 }
