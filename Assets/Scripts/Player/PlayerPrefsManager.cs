a7a4309dea6e00f143ffe0fc35faf8ea8630b571
NoahForTW <111335518+NoahForTW@users.noreply.github.com>
2023-08-21 16:52:29 +0800

    Tried to implement keybind

---
+++ Assets/Scripts/Player/PlayerPrefsManager.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public static class PlayerPrefsManager
+{
+    public static void Save(string key, float value)
+    {
+
+    }
+
+    public static float Load(string key)
+    {
+
+    }
+}
---
+++ Assets/Scripts/UI/KeybindMenu.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class KeybindMenu : MonoBehaviour
+{
+    private Dictionary<string, KeyCode> keys = new Dictionary<string, KeyCode>();
+    [Header("Keys")]
+    [SerializeField] private TMP_Text up, down, left, right, interact;
+
+    [Header("Menu Elements")]
+    [SerializeField] private GameObject keybindMenu;
+
+    private GameObject currKey;
+    private bool keybindIsOpened = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        keys.Add("Up", (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("Up", "W")));
+        keys.Add("Down", (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("Down", "S")));
+        keys.Add("Left", (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("Left", "A")));
+        keys.Add("Right", (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("Right", "D")));
+        keys.Add("Interact", (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("Interact", "E")));
+
+        up.text = keys["Up"].ToString();
+        down.text = keys["Down"].ToString();
+        left.text = keys["Left"].ToString();
+        right.text = keys["Right"].ToString();
+        interact.text = keys["Interact"].ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(keys["Up"]))
+        {
+            Debug.Log("Up");
+        }
+        if (Input.GetKeyDown(keys["Down"]))
+        {
+            Debug.Log("Down");
+        }
+        if (Input.GetKeyDown(keys["Left"]))
+        {
+            Debug.Log("Left");
+        }
+        if (Input.GetKeyDown(keys["Right"]))
+        {
+            Debug.Log("Right");
+        }
+        if (Input.GetKeyDown(keys["Interact"]))
+        {
+            Debug.Log("Interact");
+        }
+    }
+
+    private void OnGUI()
+    {
+        if (currKey != null)
+        {
+            Event e = Event.current;
+            if (e.isKey)
+            {
+                keys[currKey.name] = e.keyCode;
+                currKey.transform.GetChild(0).GetComponent<TMP_Text>().text = e.keyCode.ToString();
+                currKey = null;
+            }
+        }
+    }
+
+    public void ChangeKey(GameObject clicked)
+    {
+        currKey = clicked;
+    }
+
+    public void SaveKeys()
+    {
+        foreach(var key in keys)
+        {
+            PlayerPrefs.SetString(key.Key, key.Value.ToString());
+        }
+
+        PlayerPrefs.Save();
+    }
+}
