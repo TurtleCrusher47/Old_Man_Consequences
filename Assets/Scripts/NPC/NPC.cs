a8d7de47b62f21158eaf460c6f8e94d47b1dbed7
TurtleCrusher47 <111266586+TurtleCrusher47@users.noreply.github.com>
2023-08-21 14:46:52 +0800

    Basic NPC

--- Assets/Scripts/NPC/NPC.cs
+++ Assets/Scripts/NPC/NPC.cs
@@ -1,18 +1,91 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class NPC : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject dialoguePanel;
+    public TextMeshProUGUI dialogueText;
+    public string[] dialogue;
+    private int index;
 
+    public GameObject nextLineButton;
+    public float wordSpeed;
+    public bool inRange;
+    
     // Update is called once per frame
     void Update()
     {
-        
+        if (inRange)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                if (dialoguePanel.activeInHierarchy)
+                {
+                    ResetText();
+                }
+                else
+                {
+                    dialoguePanel.SetActive(true);
+                    StartCoroutine(Typing());
+                }
+            }
+        }
+
+        if (dialogueText.text == dialogue[index])
+        {
+            nextLineButton.SetActive(true);
+        }
+    }
+
+    public void ResetText()
+    {
+        dialogueText.text = "";
+        index = 0;
+        dialoguePanel.SetActive(false);
+    }
+
+    IEnumerator Typing()
+    {
+        foreach (char letter in dialogue[index].ToCharArray())
+        {
+            dialogueText.text += letter;
+            yield return new WaitForSeconds(wordSpeed);
+        }
+    }
+
+    public void NextLine()
+    {
+        nextLineButton.SetActive(false);
+
+        if (index < dialogue.Length - 1)
+        {
+            index ++;
+            dialogueText.text = "";
+            StartCoroutine(Typing());
+        }
+        else
+        {
+            ResetText();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            inRange = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            inRange = false;
+            ResetText();
+        }
     }
 }
