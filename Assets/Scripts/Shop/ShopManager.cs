2304a6a03d5733b3047be98f925e1936cafc433e
TurtleCrusher47 <111266586+TurtleCrusher47@users.noreply.github.com>
2023-08-22 16:29:11 +0800

    Updates whether the player can buy after every sell

--- Assets/Scripts/Shop/ShopManager.cs
+++ Assets/Scripts/Shop/ShopManager.cs
@@ -154,6 +154,7 @@ public class ShopManager : MonoBehaviour
             
             //UpdateInventoryList();
             UpdateBalance();
+            CheckPurchaseable();
             //CheckSellable();
         }
 
