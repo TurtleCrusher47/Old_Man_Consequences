07f5f8046c7c85fd5eb4a77f29ef614928067681
TurtleCrusher47 <111266586+TurtleCrusher47@users.noreply.github.com>
2023-08-24 14:16:37 +0800

    Base notification system

--- Assets/Scripts/NPC/DebtManager.cs
+++ Assets/Scripts/NPC/DebtManager.cs
@@ -6,21 +6,57 @@ public class DebtManager : MonoBehaviour
 {
     [SerializeField] private PlayerData playerData;
 
+    [SerializeField] private NPC nPC;
+
     public void BorrowOneThousand()
     {
-        playerData.Balance += 1000;
-        playerData.SharkDebt += 1000;
+        // If the player owes money
+        if (DebtCheck())
+        {
+            nPC.DisableChoice();
+        }
+        else
+        {
+            playerData.Balance += 1000;
+            playerData.SharkDebt += 1000;
+        }
     }
 
     public void BorrowFiveThousand()
     {
-        playerData.Balance += 5000;
-        playerData.SharkDebt += 5000;
+        // If the player owes money
+        if (DebtCheck())
+        {
+            nPC.DisableChoice();
+        }
+        else
+        {
+            playerData.Balance += 5000;
+            playerData.SharkDebt += 5000;
+        }
     }
 
     public void BorrowTenThousand()
     {
-        playerData.Balance += 10000;
-        playerData.SharkDebt += 10000;
+        // If the player owes money
+        if (DebtCheck())
+        {
+            nPC.DisableChoice();
+        }
+        else
+        {
+            playerData.Balance += 10000;
+            playerData.SharkDebt += 10000;
+        }
+
+    }
+
+    // Whether the player owes money or not
+    public bool DebtCheck()
+    {
+        if (playerData.SharkDebt > 0)
+        return true;
+        else
+        return false;
     }
 }
