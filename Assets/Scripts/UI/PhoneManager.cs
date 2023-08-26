d3df746d2a3a8005a981f65070a1a2b3715fad35
TurtleCrusher47 <111266586+TurtleCrusher47@users.noreply.github.com>
2023-08-27 04:31:09 +0800

    Bank app done

--- Assets/Scripts/UI/PhoneManager.cs
+++ Assets/Scripts/UI/PhoneManager.cs
@@ -6,13 +6,28 @@ using TMPro;
 public class PhoneManager : MonoBehaviour
 {
     [SerializeField] TMP_Text weekText;
+    [SerializeField] TMP_Text balanceText;
+    [SerializeField] TMP_Text bankDebtText;
+
     [SerializeField] Animator animator;
 
     [SerializeField] WorldClockData worldClockData;
+    [SerializeField] PlayerData playerData;
+
+    private void Start()
+    {
+        UpdateBalanceAndDebtText();
+    }
 
     public void UpdateWeekText()
     {
         weekText.text = "Week " + worldClockData.currentWeek.ToString();
+    }
+
+    public void UpdateBalanceAndDebtText()
+    {
+        balanceText.text = "Balance: " + playerData.Balance.ToString();
+        bankDebtText.text = "Debt: " + playerData.BankDebt.ToString();
     }
 
     public void TurnOnPhone()
